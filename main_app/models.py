b192d2dee4f9623b3f9013b262af21961edd1594
Ronnie95 <t_daron@yahoo.com>
2023-07-10 18:44:35 -0400

    updates

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Diet(models.Model):
+    breakfast = models.CharField(max_length=250)
+    lunch = models.CharField(max_length=250)
+    dinner = models.CharField(max_length=250)
+    postworkout = models.CharField(max_length=250)
+    snack = models.CharField(max_length=250)
+    hydration = models.CharField(max_length=250)
+
